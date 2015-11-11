0b6210144d213eaeb649575e22d0c2e376162633
Kevin Schmittle <kevin@schmittle.net>
2015-11-11 10:47:07 -0700

    download points

--- webui/data.php
+++ webui/data.php
@@ -1,6 +1,9 @@
 <?php
 	if (isset($_POST['points'])) {
-		echo 'Points';
+		header("Content-Type: application/force-download");
+		header("Content-Length: ".strlen($_POST['points']));
+		header("Connection: close");
+		echo $_POST['points'];
 	} else {
 		echo 'No data received';
 	}
