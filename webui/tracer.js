e3a9dec776b60ec9adcc735e6b7fce92978e85cd
Kevin Schmittle <kevin@schmittle.net>
2015-11-11 09:31:17 -0700

    initial tracer web gui

---
+++ webui/tracer.js
@@ -0,0 +1,48 @@
+// tracer.js
+
+var Tracer = function(canvas) {
+	this.canvas = canvas;
+	this.canvasOffset = this.canvas.offset();
+	console.log(this.canvasOffset);
+
+	this.ctx = this.canvas.get(0).getContext("2d");
+
+	var ctx = this.ctx;
+	ctx.rect(50, 50, this.canvas.width() - 100, this.canvas.height() - 100);
+	ctx.stroke();
+
+	this.points = [];
+
+	function handleTouchStart(o) { return function(e) {
+		e.preventDefault();
+
+		this.points = [];
+		console.log("Starting path");
+	}};
+
+	function handleTouchMove(o) { return function(e) {
+		e.preventDefault();
+
+		var touch = e.targetTouches[0];
+		var x = touch.pageX - o.canvasOffset.left;
+		var y = touch.pageY - o.canvasOffset.top;
+
+		this.points.push([x,y]);
+		console.log("pos (" + x.toString() + ", " + y.toString() + ")");
+		var ctx = o.ctx;
+		//ctx.fillStyle = "rgb(0,0,255)";
+		ctx.fillRect(x, y, 1, 1);
+	}};
+
+	function handleTouchEnd(o) { return function(e) {
+		e.preventDefault();
+
+		console.log("Finished path");
+	}};
+
+	this.canvas.get(0).addEventListener("touchstart", handleTouchStart(this));
+	this.canvas.get(0).addEventListener("touchmove", handleTouchMove(this));
+	this.canvas.get(0).addEventListener("touchend", handleTouchEnd(this));
+	
+	console.log("created Tracer");
+}
