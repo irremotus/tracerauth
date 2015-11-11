7a18364dd62173067709b8aa3f3f02934f2cb049
Kevin Schmittle <kevin@schmittle.net>
2015-11-11 10:50:28 -0700

    points file content disposition

--- webui/data.php
+++ webui/data.php
@@ -1,5 +1,6 @@
 <?php
 	if (isset($_POST['points'])) {
+		header('Content-Disposition: attachment; filename="points.dec"');
 		header("Content-Type: application/force-download");
 		header("Content-Length: ".strlen($_POST['points']));
 		header("Connection: close");
