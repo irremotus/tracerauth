b61655b7731fb8b6af13ffa721de6291a1a6ccde
Kevin Schmittle <kevin@schmittle.net>
2015-12-02 15:13:45 -0700

    added response form and database stuff

--- webui/data.php
+++ webui/data.php
@@ -4,9 +4,47 @@
 		if (isset($_POST['touch'])) {
 			$type = $_POST['touch'];
 		}
-		foreach ($traces as $traces) {
+		if (isset($_POST['traces'])) {
+			$traces = $_POST['traces'];
+		}
+		foreach ($traces as $trace) {
 			
 		}
+
+		$a_configs = array();
+		$filename = dirname(__FILE__)."/mysql_config.ini";
+		if (file_exists($filename)) {
+			$a_configs = parse_ini_file($filename);
+		}
+		if (!isset($a_configs["host"]) ||
+			!isset($a_configs["user"]) ||
+			!isset($a_configs["password"])) {
+			return FALSE;
+		}
+
+		$mysqli = new mysqli($a_configs["host"], $a_configs["user"], $a_configs["password"], "tracer");
+		if ($mysqli->connect_errno) {
+			die("Error connecting to database");
+		}
+
+		if (!($stmt = $mysqli->prepare("INSERT INTO paths(istouch, path) VALUES(?, ?);"))) {
+			die("Error writing to database");
+		}
+
+		$istouch = 0;
+		if ($type) {
+			$istouch = 1;
+		}
+
+		if (!$stmt->bind_param("is", $istouch, $path)) {
+			die("Error binding parameters");
+		}
+
+		if (!$stmt->execute()) {
+			die("Error executing insert");
+		}
+
+		echo "Success";
 	} else {
 		echo 'No data received';
 	}
