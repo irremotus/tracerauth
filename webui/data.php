c80561a43d047284741d42f437c876786239966a
Kevin Schmittle <kevin@schmittle.net>
2015-12-04 01:39:25 -0700

    more database stuff

--- webui/data.php
+++ webui/data.php
@@ -1,4 +1,12 @@
 <?php
+	session_start();
+	if (isset($_SESSION['id'])) {
+		$id = $_SESSION['id'];
+	} else {
+		$id = random_int();
+		$_SESSION['id'] = $id;
+	}
+
 	if (isset($_POST['data'])) {
 		$type = 'mouse';
 		if (isset($_POST['touch'])) {
@@ -7,28 +15,30 @@
 		if (isset($_POST['data'])) {
 			$traces = $_POST['data'];
 		}
+		$path = "";
 		foreach ($traces as $trace) {
-			echo $trace;
+			$str .= $trace . "|";
 		}
 
-		/*
 		$a_configs = array();
-		$filename = dirname(__FILE__)."/mysql_config.ini";
+		$filename = "/var/www/tracer_mysql_config.ini";
 		if (file_exists($filename)) {
 			$a_configs = parse_ini_file($filename);
+		} else {
+			die("Could not find config file");
 		}
 		if (!isset($a_configs["host"]) ||
 			!isset($a_configs["user"]) ||
-			!isset($a_configs["password"])) {
-			return FALSE;
+			!isset($a_configs["pass"])) {
+				die("Error in config file");
 		}
 
-		$mysqli = new mysqli($a_configs["host"], $a_configs["user"], $a_configs["password"], "tracer");
+		$mysqli = new mysqli($a_configs["host"], $a_configs["user"], $a_configs["pass"], "tracer");
 		if ($mysqli->connect_errno) {
 			die("Error connecting to database");
 		}
 
-		if (!($stmt = $mysqli->prepare("INSERT INTO paths(istouch, path) VALUES(?, ?);"))) {
+		if (!($stmt = $mysqli->prepare("INSERT INTO paths(sessid, istouch, path) VALUES(?, ?, ?);"))) {
 			die("Error writing to database");
 		}
 
@@ -37,14 +47,13 @@
 			$istouch = 1;
 		}
 
-		if (!$stmt->bind_param("is", $istouch, $path)) {
+		if (!$stmt->bind_param("iis", $id, $istouch, $path)) {
 			die("Error binding parameters");
 		}
 
 		if (!$stmt->execute()) {
 			die("Error executing insert");
 		}
-		 */
 
 		echo "Success";
 	} else {
