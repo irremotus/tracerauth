7e61c55486ae97af0ce9ab7a56b2fff4181c01ed
Kevin Schmittle <kevin@schmittle.net>
2015-12-05 19:21:15 -0700

    updated files

--- pytest/parse.py
+++ pytest/parse.py
@@ -1,6 +1,7 @@
 import os
 
 f = open('data')
+f2 = open('data2', 'w')
 
 lines = f.readlines()
 
@@ -8,14 +9,17 @@ i = 1
 for line in lines:
 	paths = line.replace('><', "\n").splitlines()
 	if len(paths) > 1:
+		f2.write(line + "\n")
 		dirname = "subject" + str(i)
 		i += 1
 		os.makedirs(dirname)
 		j = 1
 		for path in paths:
-			fo = open(dirname + "/" + str(j) + '.txt', 'w')
-			j += 1
 			path = path.replace('<', ' ').replace('>', ' ')
 			path = path.strip()
-			fo.write(path + "\n")
-			fo.close()
+			if len(path) > 0:
+				fo = open(dirname + "/" + str(j) + '.txt', 'w')
+				fo.write(path + "\n")
+				fo.close()
+			j += 1
+f2.close()
